777c6dc5d3981ff2c24e2314345bd72e96606faa
letme-code <97297339+letme-code@users.noreply.github.com>
2022-02-04 20:47:08 +0300

    Update KEY_SIMULATE.cpp

--- KEY_SIMULATE.cpp
+++ KEY_SIMULATE.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include <vector>
+#include <winuser.h>
 
 void LeftClick ( );
 void MouseMove ( int x, int y );
@@ -7,25 +8,46 @@ void Q_KEY();
 void RightClick ( );
 void D_KEY();
 void A_KEY();
-void E_KEY();
-
+void one_KEY();
+void EMPTY_KEY();
+void UC_KEY();
+void iki_KEY();
+void OLTA_KEY();
 
 int main(){
+
     while(true){
-        if(GetAsyncKeyState(VK_NUMPAD0)){
-            return 0;
+    //  TRIGGER  F
+        if(GetAsyncKeyState(0X46)){
+      
+            Sleep(71);
+           one_KEY();
+           LeftClick();
+          
         }
 
-        
-    //  TRIGGER F
-        if(GetAsyncKeyState(0x46)){
+//trigger G
+          if(GetAsyncKeyState(0X47)){
+              Sleep(71);
+            iki_KEY();
+          RightClick();
             
         }
 
-    //  TRIGGER  Z 
-        if(GetAsyncKeyState(0x5A)){
-            LeftClick();
-            A_KEY();
+//trigger X
+          if(GetAsyncKeyState(0X58)){
+              Sleep(71);
+              UC_KEY();
+              RightClick();
+        }
+        //combo
+         if(GetAsyncKeyState(0X52)){
+              Sleep(71);
+              UC_KEY();
+              RightClick();
+              Sleep(15);
+              one_KEY();
+              LeftClick();
         }
 
     }
@@ -43,28 +65,102 @@ void Q_KEY(){
             input.ki.dwFlags = KEYEVENTF_SCANCODE;
             input.ki.wScan = mappedkey;
             SendInput(1, &input, sizeof(input));
-            Sleep(75);
             input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
             SendInput(1, &input, sizeof(input));
   
 }
 
-
-void E_KEY(){
+void D_KEY(){
             SHORT key;
             UINT mappedkey;
             INPUT input = { 0 };
-            key = VkKeyScan('e');
+            key = VkKeyScan('d');
             mappedkey = MapVirtualKey(LOBYTE(key), 0);
             input.type = INPUT_KEYBOARD;
             input.ki.dwFlags = KEYEVENTF_SCANCODE;
             input.ki.wScan = mappedkey;
             SendInput(1, &input, sizeof(input));
-            Sleep(75);
             input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
             SendInput(1, &input, sizeof(input));
   
 }
+
+void OLTA_KEY(){
+            SHORT key;
+            UINT mappedkey;
+            INPUT input = { 0 };
+            key = VkKeyScan('3');
+            mappedkey = MapVirtualKey(LOBYTE(key), 0);
+            input.type = INPUT_KEYBOARD;
+            input.ki.dwFlags = KEYEVENTF_SCANCODE;
+            input.ki.wScan = mappedkey;
+            SendInput(1, &input, sizeof(input));
+            input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+            SendInput(1, &input, sizeof(input));
+  
+}
+
+
+void one_KEY(){
+            SHORT key;
+            UINT mappedkey;
+            INPUT input = { 0 };
+            key = VkKeyScan('1');
+            mappedkey = MapVirtualKey(LOBYTE(key), 0);
+            input.type = INPUT_KEYBOARD;
+            input.ki.dwFlags = KEYEVENTF_SCANCODE;
+            input.ki.wScan = mappedkey;
+            SendInput(1, &input, sizeof(input));
+            input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+            SendInput(1, &input, sizeof(input));
+  
+}
+
+void UC_KEY(){
+            SHORT key;
+            UINT mappedkey;
+            INPUT input = { 0 };
+            key = VkKeyScan('4');
+            mappedkey = MapVirtualKey(LOBYTE(key), 0);
+            input.type = INPUT_KEYBOARD;
+            input.ki.dwFlags = KEYEVENTF_SCANCODE;
+            input.ki.wScan = mappedkey;
+            SendInput(1, &input, sizeof(input));
+            input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+            SendInput(1, &input, sizeof(input));
+  
+}
+
+void iki_KEY(){
+            SHORT key;
+            UINT mappedkey;
+            INPUT input = { 0 };
+            key = VkKeyScan('2');
+            mappedkey = MapVirtualKey(LOBYTE(key), 0);
+            input.type = INPUT_KEYBOARD;
+            input.ki.dwFlags = KEYEVENTF_SCANCODE;
+            input.ki.wScan = mappedkey;
+            SendInput(1, &input, sizeof(input));
+            input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+            SendInput(1, &input, sizeof(input));
+  
+}
+
+void EMPTY_KEY(){
+            SHORT key;
+            UINT mappedkey;
+            INPUT input = { 0 };
+            key = VkKeyScan(' ');
+            mappedkey = MapVirtualKey(LOBYTE(key), 0);
+            input.type = INPUT_KEYBOARD;
+            input.ki.dwFlags = KEYEVENTF_SCANCODE;
+            input.ki.wScan = mappedkey;
+            SendInput(1, &input, sizeof(input));
+            input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+            SendInput(1, &input, sizeof(input));
+  
+}
+
 
 void A_KEY(){
        SHORT key;
@@ -81,21 +177,6 @@ void A_KEY(){
             SendInput(1, &input, sizeof(input));
 }
 
-
-void D_KEY(){
-         SHORT key;
-            UINT mappedkey;
-            INPUT input = { 0 };
-            key = VkKeyScan('d');
-            mappedkey = MapVirtualKey(LOBYTE(key), 0);
-            input.type = INPUT_KEYBOARD;
-            input.ki.dwFlags = KEYEVENTF_SCANCODE;
-            input.ki.wScan = mappedkey;
-            SendInput(1, &input, sizeof(input));
-            Sleep(75);
-            input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-            SendInput(1, &input, sizeof(input));
-}
 
 
 
